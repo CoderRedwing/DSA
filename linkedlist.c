cfe38d840c24fc0c08b716f3d18e65ed265ff73c
CoderRedwing <ajiteshru@gmail.com>
2023-02-18 23:38:12 +0530

    stack

--- linkedlist.c
+++ linkedlist.c
@@ -1,12 +1,38 @@
 #include<stdio.h>
+#include<stdlib.h>
+int main(){
+  
   struct node
   {
-    int data;
-    struct node *Next;
-
+    int info;
+    struct node *link;
   };
-  int main(){
-   printf("h");
+  struct node *START=NULL;
+  struct node *createNode(){
+    struct node *n;
+    n=(struct node*)malloc(sizeof(struct node));
+    return(n);
+  
   }
+
+  void insertNode(){
+    struct node *temp,*t;
+    temp =createNode();
+    printf("enter a number ");
+    scanf("%d",&temp->info);
+    temp->link=NULL;
+    if(START==NULL)
+    START=temp;
+     else{
+      t=START;
+      while(t->link!=NULL)
+      t=t->link;
+      t->link=temp;
+  
+     }
+      }
+        
+}
+  
   
    
