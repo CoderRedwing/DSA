5abe1e2948c0cf118c6fe9ffdfae1a5e0c87e342
CoderRedwing <ajiteshru@gmail.com>
2023-02-23 23:18:58 +0530

    li

--- linkedlist.c
+++ linkedlist.c
@@ -1,7 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
-  
+int insertNode();
+void viewlist();
+int main()
+{
+  int choice;
+
+  while(1)
+  {
+    printf("1.Insert \n");
+    printf("2.view \n");
+    printf("3.exit\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+      case 1:
+      insertNode();
+      break;
+      case 2:
+      viewlist();
+      break;
+      case 3:
+      exit(0);
+      default :
+      printf("You Entered wrong choice");
+    }
+  }
+}
   struct node
   {
     int info;
@@ -9,13 +34,13 @@ int main(){
   };
   struct node *START=NULL;
   struct node *createNode(){
-    struct node *n;
-    n=(struct node*)malloc(sizeof(struct node));
-    return(n);
-  
-  }
+  struct node *n;
+  n=(struct node*)malloc(sizeof(struct node));
+  return(n);
 
-  void insertNode(){
+  }
+  int  insertNode()
+  {
     struct node *temp,*t;
     temp =createNode();
     printf("enter a number ");
@@ -23,16 +48,31 @@ int main(){
     temp->link=NULL;
     if(START==NULL)
     START=temp;
-     else{
+     else
+     {
       t=START;
       while(t->link!=NULL)
-      t=t->link;
-      t->link=temp;
-  
-     }
+      {
+         t=t->link;
       }
-        
+     } 
+     t->link=temp;
+     return 0;
+  }
+void viewlist()
+{
+  struct node *t;
+  t=START;
+  printf("Entered data \n");
+  while(t!=NULL)
+  {
+    printf( "%d ",t->info);
+    t=t->link;
+  }
+  printf("\n");
 }
+        
+
   
   
    
