8aae3a032d6bba5e4b1f3e6e6634f27645c9ed48
CoderRedwing <ajiteshru@gmail.com>
2023-04-16 00:55:21 +0530

    str

---
+++ sum.cpp
@@ -0,0 +1,39 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n,i,sum=0;
+    cout<<"Enter number";
+    cin>>n;
+    for(i=1; i<=n; i++)
+    sum = sum+i;
+    cout<<sum<<endl;
+    return 0;
+}*/
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n,r,product=1,sum=0;
+    cin>>n;
+    while ((n>0))
+    {
+        r=n%10;
+        if(r%2==0){
+         sum = sum+r;
+        }
+        else{
+        product = product*r;
+        
+        }
+      n=n/10;  
+    }
+    cout<<sum<<endl;
+   cout<<product<<endl;
+
+
+}*/
+#include<iostream>
+using namespace std;
+int main(){
+    cin>>n;
+    
+}
